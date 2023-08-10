ad33a1e039bf3cbc94bd940ddb8d3aaa7af57b46
yinyuao <yinyuao@kingsoft.com>
2023-08-10 16:55:51 +0800

    add:zk配置类

--- easyClient/src/main/java/com/ksyun/campus/client/util/ZkUtil.java
+++ easyClient/src/main/java/com/ksyun/campus/client/util/ZkUtil.java
@@ -1,10 +1,22 @@
 package com.ksyun.campus.client.util;
 
-import javax.annotation.PostConstruct;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class ZkUtil {
-    @PostConstruct
-    public void postCons() throws Exception {
-        // todo 初始化，与zk建立连接，注册监听路径，当配置有变化随时更新
+
+    @Bean(initMethod = "start", destroyMethod = "close")
+    public CuratorFramework curatorFramework() {
+        // 创建Curator客户端
+        CuratorFramework client = CuratorFrameworkFactory.builder()
+                .connectString("8.130.138.230" + ":2181")
+                .retryPolicy(new ExponentialBackoffRetry(1000, 3))
+                .build();
+        return client;
     }
+
 }
