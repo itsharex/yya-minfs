bdfa6af775374342716c8ec1196130faa2c87893
yinyuao <yinyuao@kingsoft.com>
2023-08-11 16:42:31 +0800

    add:增加zk配置类

--- dataServer/src/main/java/com/ksyun/campus/dataserver/config/ZooKeeperConfig.java
+++ dataServer/src/main/java/com/ksyun/campus/dataserver/config/ZooKeeperConfig.java
@@ -3,17 +3,21 @@ package com.ksyun.campus.dataserver.config;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ZooKeeperConfig {
 
+    @Value("${zookeeper.addr:10.0.0.201:2181}")
+    private String zookeeperAddr;
+
     @Bean(initMethod = "start", destroyMethod = "close")
     public CuratorFramework curatorFramework() {
         // 创建Curator客户端
         CuratorFramework client = CuratorFrameworkFactory.builder()
-                .connectString("8.130.138.230" + ":2181")
+                .connectString(zookeeperAddr)
                 .retryPolicy(new ExponentialBackoffRetry(1000, 3))
                 .build();
         return client;
--- metaServer/src/main/java/com/ksyun/campus/metaserver/config/ZooKeeperConfig.java
+++ metaServer/src/main/java/com/ksyun/campus/metaserver/config/ZooKeeperConfig.java
@@ -3,6 +3,7 @@ package com.ksyun.campus.metaserver.config;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
@@ -10,11 +11,15 @@ import org.springframework.stereotype.Component;
 @Configuration
 public class ZooKeeperConfig {
 
+    @Value("${zookeeper.addr:10.0.0.201:2181}")
+    private String zookeeperAddr;
+
     @Bean(initMethod = "start", destroyMethod = "close")
     public CuratorFramework curatorFramework() {
+        System.out.println(zookeeperAddr);
         // 创建Curator客户端
         CuratorFramework client = CuratorFrameworkFactory.builder()
-                .connectString("8.130.138.230" + ":2181")
+                .connectString(zookeeperAddr)
                 .retryPolicy(new ExponentialBackoffRetry(1000, 3))
                 .build();
         return client;
