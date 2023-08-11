c3ff972709e8ca3a7e7c05ba89d8605db32d127c
yinyuao <yinyuao@kingsoft.com>
2023-08-11 16:43:37 +0800

    add:增加缓存存储dataServe信息

--- metaServer/src/main/java/com/ksyun/campus/metaserver/cache/ServerInfoCache.java
+++ metaServer/src/main/java/com/ksyun/campus/metaserver/cache/ServerInfoCache.java
@@ -1,11 +1,10 @@
 package com.ksyun.campus.metaserver.cache;
 
+import com.ksyun.campus.metaserver.domain.ReplicaData;
 import com.ksyun.campus.metaserver.entity.ServerInfo;
 import org.springframework.stereotype.Component;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @Component
 public class ServerInfoCache {
@@ -37,6 +36,31 @@ public class ServerInfoCache {
 
     public Collection<String> getAllPaths() {
         return cache.keySet();
+    }
+
+    public List<ReplicaData> getRandomServerInfos(int count) {
+        List<ReplicaData> result = new ArrayList<>();
+
+        List<String> keys = new ArrayList<>(cache.keySet());
+        Random random = new Random();
+
+        int keysCount = keys.size();
+        if (count > keysCount) {
+            count = keysCount; // 保证不超过缓存中的数量
+        }
+
+        for (int i = 0; i < count; i++) {
+            int randomIndex = random.nextInt(keysCount);
+            String randomKey = keys.get(randomIndex);
+            ServerInfo serverInfo = cache.get(randomKey);
+            ReplicaData replicaData = new ReplicaData();
+            replicaData.setPath(randomKey);
+            replicaData.setId(serverInfo.getRack() + "_" + serverInfo.getZone());
+            replicaData.setDsNode(serverInfo.getIp() + ":" + serverInfo.getPort());
+            result.add(replicaData);
+        }
+
+        return result;
     }
 
 }
